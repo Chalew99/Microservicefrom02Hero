b33242ec31a151ec034f5ca403c6dffefaf42538
v-chmeha <v-chmeha@microsoft.com>
2022-10-24 07:50:14 -0700

    Push my local version to git for the first time

---
+++ OrderApi/RequestRabbitMQ Application/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace RequestRabbitMQ_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName = "futurepast1977";
+
+            string Password = "redmondmicrosot123";
+
+            string HostName = "localhost";
+
+
+
+            //Main entry point to the RabbitMQ .NET AMQP client
+
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory()
+
+            {
+
+                UserName = UserName,
+
+                Password = Password,
+
+                HostName = HostName
+
+            };
+
+
+
+            var connection = connectionFactory.CreateConnection();
+
+            var model = connection.CreateModel();
+
+            Console.WriteLine("Creating Exchange");
+
+            // Create Exchange
+
+            model.ExchangeDeclare("demoExchange", ExchangeType.Direct);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
